1b5c473d2ece4cbab57117742023b9c2a728d5ae
Paradise-Dami <imbenkelfat@gmail.com>
2025-01-26 19:16:53 +0100

    bon

---
+++ info 2/helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world\n");
+return 0;
+
+}
+
+
+/*imad@DESKTOP-B563F4M:/mnt/d/github/Cours-Sorbonne-Imad/info 2$ ls
+"dossier des tme d'info.txt"   helloworld.c
+imad@DESKTOP-B563F4M:/mnt/d/github/Cours-Sorbonne-Imad/info 2$ gcc -Wall helloworld.c
+imad@DESKTOP-B563F4M:/mnt/d/github/Cours-Sorbonne-Imad/info 2$ ls
+ a.out  "dossier des tme d'info.txt"   helloworld.c
+imad@DESKTOP-B563F4M:/mnt/d/github/Cours-Sorbonne-Imad/info 2$ ./a.out
+Hello world
+imad@DESKTOP-B563F4M:/mnt/d/github/Cours-Sorbonne-Imad/info 2$*/

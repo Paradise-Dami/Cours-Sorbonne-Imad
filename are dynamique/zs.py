40cf15a6aa3b3b835926a4e6f1e75852c2757607
Paradise-Dami <87902304+Paradise-Dami@users.noreply.github.com>
2025-01-30 19:32:38 +0100

    TME semaine 1 fini
    TME fini en attente de revue par la chargée de TME

--- are dynamique/zs.py
+++
@@ -1,4 +0,0 @@
-liste=[0,2,2,3]
-liste2=list(liste)
-liste2[0]=1
-print(liste2)
--- info 2/semaine1/Exercice 8.c
+++ info 2/semaine1/Exercice 8.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-#include <math.h>
 #include <assert.h>
+#define tarifAdulte 22.7
+#define traifEnfant 10.75
+#define tarifFamille 57.80
 
-float prixEntree(int nbAdultes, int nbEnfants, int )
+float prixEntree(int nbAdultes, int nbEnfants) {
+    float prixFinal;
+    prixFinal = 0;
+    if ((nbAdultes <= 2) && (nbEnfants <= 3)) {
+            prixFinal = prixFinal + nbAdultes*tarifAdulte + nbEnfants*traifEnfant;
+        if (prixFinal > tarifFamille) {
+            return tarifFamille;
+        } else {
+            return prixFinal;
+        }
+        } else {
+            return tarifAdulte*(nbAdultes-2) + traifEnfant*(nbEnfants-3) + tarifFamille;
+        }
+            return prixFinal;
+    }
+
+
+
+
+int main() {
+    printf("prixEntree(%d,%d) = %f\n", 2, 3, prixEntree(2,3));
+    printf("prixEntree(%d,%d) = %f\n", 2, 2, prixEntree(2,2));
+    printf("prixEntree(%d,%d) = %f\n", 2, 1, prixEntree(2,1));
+    printf("prixEntree(%d,%d) = %f\n", 1, 3, prixEntree(1,3));
+    return 0;
+}

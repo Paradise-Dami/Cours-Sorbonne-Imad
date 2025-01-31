26b0a34e3fb84b3763b91d9ba71ee8b3b27a852b
Paradise-Dami <87902304+Paradise-Dami@users.noreply.github.com>
2025-01-31 20:19:27 +0100

    TME semaine 1 fini
    Tout les fichiers ont été envoyés sur CodeRunner

--- info 2/tme/semaine1/Exercice 5.c
+++ info 2/tme/semaine1/Exercice 5.c
@@ -9,24 +9,30 @@ int discriminant(int a, int b, int c) {
 
 //Question 2
 
-float afficheRacine(int a, int b, int c) {
+void afficheRacines(int a, int b, int c) {
     float x1;
     float x2;
     int delta;
     delta = discriminant(a,b,c);
-    x1 = (-b-sqrt(delta))/(2*a);
-    x2 = (-b+sqrt(delta))/(2*a);
-    printf("x1 = %f, x2 = %f\n",x1,x2);
-    return x1,x2;
+    if (delta < 0) {
+        printf("Pas de racine reelle\n");
+    } else {
+        x1 = (-b+sqrt(delta))/(2*a);
+        x2 = (-b-sqrt(delta))/(2*a);
+        if (delta == 0) {
+            printf("La racine double est %.2f\n",x1);
+        } else {
+        printf("Les deux racines sont %.2f et %.2f\n",x1,x2);
+    }
+    }
 }
 
 
 int main(){
     //Question 3
-    printf("%d\n",discriminant(4,4,1));
-    afficheRacine(4,4,1);
-    afficheRacine(4,6,1); 
-    afficheRacine(-7,-5,-1);
+    afficheRacines(2,5,3);
+    afficheRacines(4,6,1); 
+    afficheRacines(-7,-5,-1);
     return 0;
 }
 
--- info 2/tme/semaine1/Exercice 8.c
+++ info 2/tme/semaine1/Exercice 8.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
-#define tarifAdulte 22.7
-#define traifEnfant 10.75
-#define tarifFamille 57.80
+#define TADULTE 22.7
+#define TENFANT 10.75
+#define TFAMILLE 57.80
 
 float prixEntree(int nbAdultes, int nbEnfants) {
-    float prixFinal;
-    prixFinal = 0;
-    if ((nbAdultes <= 2) && (nbEnfants <= 3)) {
-            prixFinal = prixFinal + nbAdultes*tarifAdulte + nbEnfants*traifEnfant;
-        if (prixFinal > tarifFamille) {
-            return tarifFamille;
+    float prixFinal = 0;
+    prixFinal = nbAdultes*TADULTE + nbEnfants*TENFANT;
+    float prixFinalTarifFamille;
+    if (nbAdultes > 2) {
+        if (nbEnfants > 3) {
+            prixFinalTarifFamille = (nbAdultes-2)*TADULTE + (nbEnfants-3)*TENFANT + TFAMILLE;
         } else {
-            return prixFinal;
+            prixFinalTarifFamille = (nbAdultes-2)*TADULTE + TFAMILLE;
         }
+    } else {
+        if (nbEnfants >= 3) {
+            prixFinalTarifFamille = (nbEnfants-3)*TENFANT + TFAMILLE;
         } else {
-            return tarifAdulte*(nbAdultes-2) + traifEnfant*(nbEnfants-3) + tarifFamille;
+            prixFinalTarifFamille = TFAMILLE;
         }
-            return prixFinal;
     }
-
-
-
+    if (prixFinal > prixFinalTarifFamille) {
+        return prixFinalTarifFamille;
+    } else {
+        return prixFinal;
+    }
+        }
 
 int main() {
-    printf("prixEntree(%d,%d) = %0.1f\n", 2, 3, prixEntree(2,3));
-    printf("prixEntree(%d,%d) = %0.1f\n", 2, 2, prixEntree(2,2));
-    printf("prixEntree(%d,%d) = %0.1f\n", 2, 1, prixEntree(2,1));
-    printf("prixEntree(%d,%d) = %0.1f\n", 1, 3, prixEntree(1,3));
+    printf("prixEntree(%d,%d) = %0.2f\n", 2, 3, prixEntree(2,3));
+    printf("prixEntree(%d,%d) = %0.2f\n", 6, 3, prixEntree(6,3));
+    printf("prixEntree(%d,%d) = %0.2f\n", 2, 1, prixEntree(1,3));
+    printf("prixEntree(%d,%d) = %0.2f\n", 5, 7, prixEntree(5,7));
+    printf("prixEntree(%d,%d) = %0.2f\n", 10, 0, prixEntree(10,0));
+    printf("prixEntree(%d,%d) = %0.2f\n", 0, 4, prixEntree(0,4));
     return 0;
 }

dbe30de664601791031e8057430968e362af2122
Paradise-Dami <87902304+Paradise-Dami@users.noreply.github.com>
2025-02-21 22:19:56 +0100

    TME SEMAINE 4 FINI
    rendu

--- info 2/tme/semaine4/Exercice 26.c
+++ info 2/tme/semaine4/Exercice 26.c
@@ -1,32 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // question 1
-void fusion(int *tab1, int *tab2, int taille1, int taille2, int *tab3)
+/*void fusion(int *tab1, int *tab2, int taille1, int taille2, int *tab3)
 {
     int i = 0, j = 0;
-    while ((i < taille1) && (j < taille2))
+    while ((i + j) < (taille1 + taille2))
     {
-        if ((tab1[i] <= tab2[j]) && (i < taille1))
+        if (i < taille1 + 1)
         {
-            tab3[i] = tab1[i];
-            printf("%da\n",tab3[i + j]);
-            i++;
+
+            if (tab1[i] <= tab2[j])
+            {
+                printf("aaa\n");
+                tab3[i + j] = tab1[i];
+                i++;
+            } else if (j < taille2 + 1)
+            {
+
+                if (tab1[i] > tab2[j])
+                {
+                    tab3[i + j] = tab2[j];
+                    j++;
+                }
+
+
+
+            }
         }
-        else if (j < taille2)
-        {
-            
-            tab3[i + j] = tab2[j];
-            printf("%db\n",tab3[i + j]);
-            j++;
+
+        printf("%d,%d\n", i, j);
+    }
+    printf("%d", i + j);
+}*/
+
+int* fusion(int tab1[], int tab2[], int taille1, int taille2) {
+    /* Réalise la fusion triée des tableaux triés tab1 et tab2 */
+    int* resultat = (int*)malloc((taille1 + taille2) * sizeof(int));
+    if (resultat == NULL) {
+        printf("Erreur d'allocation mémoire.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int i1 = 0, i2 = 0, i = 0;
+    
+    while (i1 < taille1 && i2 < taille2) {
+        if (tab1[i1] < tab2[i2]) {
+            resultat[i++] = tab1[i1++];
+        } else {
+            resultat[i++] = tab2[i2++];
         }
     }
+    
+    while (i1 < taille1) {
+        resultat[i++] = tab1[i1++];
+    }
+    
+    while (i2 < taille2) {
+        resultat[i++] = tab2[i2++];
+    }
+    
+    return resultat;
 }
+
 
 int main()
 {
-    int tab1[2] = {0, 2}, tab2[1] = {0}, taille1 = 2, taille2 = 1;
-    int tab3[taille1 + taille2];
-    fusion(tab1, tab2, taille1, taille2, tab3);
+    int tab1[2] = {4, 5}, tab2[2] = {0, 7}, taille1 = 2, taille2 = 2;
+    int *tab3;
+    tab3 = fusion(tab1, tab2, taille1, taille2);
     for (int k = 0; k < taille1 + taille2; k++)
     {
         printf("tab[%d] = %d\n", k, tab3[k]);

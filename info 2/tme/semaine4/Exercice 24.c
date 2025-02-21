ffaf23c813284dabadd98f3e7c30fb4f02fa5a0a
Paradise-Dami <imbenkelfat@gmail.com>
2025-02-21 14:05:56 +0100

    Exo 24 et 26
    agagagagaa

--- info 2/tme/semaine4/Exercice 24.c
+++ info 2/tme/semaine4/Exercice 24.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
-//question 1
-int indiceInsert(int *tab, int taille, int nbElements, int el)
+// question 1
+int indiceInsert(int *tab, int el, int nbElements, int taille)
 {
     int indice = 0;
     if (nbElements < taille)
     {
-        while ((indice < nbElements) && (tab[indice] > el))
+        while ((indice < nbElements) && (tab[indice] < el))
         {
             indice++;
         }
@@ -20,11 +19,36 @@ int indiceInsert(int *tab, int taille, int nbElements, int el)
     return -1;
 }
 
-//question 2
+// question 2
+
+int insertElt(int *tab, int el, int *nbElements, int taille)
+{
+    int indEl = indiceInsert(tab, taille, *nbElements, taille);
+    if (indEl == -1)
+    {
+        return 0;
+    }
+    for (int i = taille; i > indEl; i--)
+    {
+        tab[i] = tab[i - 1];
+    }
+    tab[indEl] = el;
+    *nbElements = *nbElements + 1;
+
+    return 1;
+}
+
 
 
 
 int main()
 {
+    int tab[20] = {0, 1, 3}, taille = 20, nbElements = 3, el = 10;
+    printf("%d\n", indiceInsert(tab, taille, nbElements, el));
+    insertElt(tab, el, &nbElements, taille);
+    for (int k = 0; k < nbElements; k++)
+    {
+        printf("tab[%d] = %d\n", k, tab[k]);
+    }
     return 0;
 }

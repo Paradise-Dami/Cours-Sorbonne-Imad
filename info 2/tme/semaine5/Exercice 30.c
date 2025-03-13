37f5e88f48fdd8809b9ee43e5432b859c752dbc7
Paradise-Dami <imbenkelfat@gmail.com>
2025-03-13 13:26:06 +0100

    TME semaine 6

--- info 2/tme/semaine5/Exercice 30.c
+++ info 2/tme/semaine5/Exercice 30.c
@@ -2,37 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 #define MIN 2
-#define MAX 3
+#define MAX 5
 
 // question 1
 int generertabbool(int *tab, int max, int min)
 {
     // on suppose que tab est de longeur max+1
-    int taille = rand() % (max - min + 1) + min;
-    for (int i = 0, i < taille, i++)
+    int taille = (rand() % (max - min + 1)) + min;
+    //printf("%d", taille);
+    for (int i = 0; i < taille; i++)
     {
         tab[i] = rand() % 2;
+        //printf("%d", tab[i]);
     }
-    tab[taille + 1] = -1;
+    tab[taille] = -1;
     return taille;
 }
 
-void affichertab(int *tab, taille)
+void affichertab(int *tab)
 {
     printf("[");
-    for (i = 0, i < taille, i++)
-    {
-        printf("%d ", tab[i]);
+    int i = 0;
+    while (tab[i]!=-1) {
+        printf("%d, ", tab[i]);
+        i++;
     }
     printf("]\n");
 }
 
 // question 2
-int compress_tab(int *tab_brut, int *tab_compress)
+void compress_tab(int *tab_brut, int *tab_compress)
 {
     int compteur = 0;
     int indice_tab_brut = 0;
-    int indice_tab_compresse = 0 int valprec = tab_brut[0];
+    int indice_tab_compresse = 0;
+    int valprec = tab_brut[0];
     while (tab_brut[indice_tab_brut] != -1)
     {
         if (valprec == tab_brut[indice_tab_brut])
@@ -51,21 +55,24 @@ int compress_tab(int *tab_brut, int *tab_compress)
     }
 }
 
-int decompress_tab(int *tab_brut, int tab_compress)
+void decompress_tab(int *tab_brut, int *tab_compress)
 {
     //2 chemin de la louette, lyon
     int compteur = 0;
     while (tab_compress[compteur] != -1) {
-        for (i = 0, i < tab_compress[compteur], i++) {
-
+        for (int i = 0; i < tab_compress[compteur]; i++) {
         }
     }
 }
 
 int main()
 {
+    //srand(time(NULL));
     int tabnb[MAX + 1];
     int taille = generertabbool(tabnb, MAX, MIN);
-    affichertab(tab, taille);
+    affichertab(tabnb);
+    int tabnbcompress[2*(MAX + 1)];
+    compress_tab(tabnb,tabnbcompress);
+    affichertab(tabnbcompress);
     return 0;
 }
---
+++ info 2/tme/semaine6/Exercice 32.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define N 3
+
+// question 1
+int isElementcoderunner(int *tab, int taille, int elt)
+{
+    int indiceplusgrand = 0;
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i] == elt)
+        {
+            indiceplusgrand = i;
+        }
+    }
+    if (tab[indiceplusgrand] == elt)
+    {
+        return indiceplusgrand;
+    }
+    return -1;
+}
+
+int isElement(int *tab, int taille, int elt)
+{
+    for (int i = 0; i < taille; i++)
+    {
+        if (tab[i] == elt)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int isElementrec(int *tab, int taille, int elt)
+{
+    if (taille == 0)
+    {
+        return -1;
+    }
+    if (tab[taille - 1] == elt)
+    {
+        int a = isElementrec(tab, taille - 1, elt);
+        if ((taille-1) <= a){
+            return taille-1;
+        } else {
+            return a;
+        }
+    }
+        return isElementrec(tab, taille - 1, elt);
+}
+
+int recherche_rec_aux(int *tab, int taille, int indice, int elem) {
+
+}
+
+int main()
+{
+    int tab[N] = {3, 2, 1};
+    //printf("%d", isElementrec(tab, N, 3));
+    return 0;
+}

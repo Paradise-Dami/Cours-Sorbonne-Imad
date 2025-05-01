51fad0eb01f79f4d194316ee0aed00b46935478b
Paradise-Dami <87902304+Paradise-Dami@users.noreply.github.com>
2025-05-01 17:46:19 +0200

    semaine 9 tme
    j'suis en retard mais voilà je le finis avant l'heure fatidique

--- info 2/tme/semaine9/multi_ensembles.c
+++ info 2/tme/semaine9/multi_ensembles.c
@@ -5,18 +5,15 @@
 /* Retourne un pointeur sur le premier element de valeur val, retourne NULL si aucun �l�ment n'a la valeur val */
 element_t *Recherche_val(element_t *ensemble, int val)
 {
-  if (ensemble == NULL)
+  while (ensemble != NULL)
   {
-    return NULL;
+    if (ensemble->valeur == val)
+    {
+      return ensemble;
+    }
+    ensemble = ensemble->suivant;
   }
-  if (ensemble->valeur == val)
-  {
-    return ensemble;
-  }
-  else
-  {
-    return Recherche_val(ensemble->suivant, val);
-  }
+  return NULL;
 }
 
 /* Ajoute l'element val en tete de l'ensemble s'il n'apparait pas dans l'ensemble, augmente sa frequence de 1 sinon */
@@ -24,68 +21,28 @@ element_t *Ajout_tete_ensemble(element_t *ensemble, int val, int freq)
 {
   if (ensemble == NULL)
   {
-    element_t *ensemble = malloc(sizeof(element_t));
+    ensemble = malloc(sizeof(element_t));
     ensemble->valeur = val;
     ensemble->frequence = freq;
-    ensemble->suivant = NULL;
     return ensemble;
   }
-  element_t *isTherePtr = Recherche_val(ensemble, val);
+  element_t *nouvelle_tete;
+  nouvelle_tete = malloc(sizeof(element_t));
+  element_t *is_there = Recherche_val(ensemble, val);
+  if (is_there == NULL)
+  {
+    nouvelle_tete->valeur = val;
+    nouvelle_tete->frequence = freq;
+    nouvelle_tete->suivant = ensemble;
+    return nouvelle_tete;
+  }
+  else
+  {
+    is_there->frequence = is_there->frequence + freq;
+    return ensemble;
+  }
 
-  if (isTherePtr != NULL)
-  {
-    isTherePtr->frequence = isTherePtr->frequence + freq;
-    return isTherePtr;
-  }
-  element_t *ancienneTete = malloc(sizeof(element_t));
-  ancienneTete->valeur = ensemble->valeur;
-  ancienneTete->frequence = ensemble->frequence;
-  ancienneTete->suivant = ensemble->suivant;
-  ensemble->valeur = val;
-  ensemble->frequence = freq;
-  ensemble->suivant = ancienneTete;
-  return ensemble;
-}
-
-element_t *Supprime_total_element_ensemble(element_t *ensemble, int val)
-{
-  if (ensemble == NULL)
-  {
-    return NULL;
-  }
-  if (Recherche_val(ensemble, val) == NULL)
-  {
-    return ensemble;
-  }
-  element_t *elToKill = Recherche_val(ensemble, val);
-  element_t *actuel = ensemble;
-  while (actuel->suivant != elToKill)
-  {
-    actuel = actuel->suivant;
-  }
-  actuel->suivant = elToKill->suivant;
-  free(elToKill);
-  return ensemble;
-}
-
-element_t *Supprime_element_ensemble(element_t *ensemble, int val)
-{
-  if (ensemble == NULL)
-  {
-    return NULL;
-  }
-  element_t *elToModify = Recherche_val(ensemble, val);
-  if (elToModify == NULL)
-  {
-    return ensemble;
-  }
-  if (elToModify->frequence > 1)
-  {
-    elToModify->frequence = elToModify->frequence - 1;
-    return ensemble;
-  }else {
-    return Supprime_total_element_ensemble(ensemble, val);
-  }
+  return NULL;
 }
 
 /* Affche tous les elements d'un ensemble avec leur frequence */
@@ -115,4 +72,147 @@ element_t *Creation_ensemble(int n)
     ensemble = Ajout_tete_ensemble(ensemble, val, 1);
   }
   return ensemble;
+}
+
+element_t *Supprime_total_element_ensemble(element_t *element, int val)
+{
+  if (element == NULL)
+  {
+    return NULL;
+  }
+
+  if (element->valeur == val)
+  {
+    element_t *temp = element->suivant;
+    free(element);
+    return temp;
+  }
+  element_t *el_actu = element;
+  while (el_actu->suivant != NULL)
+  {
+    if (el_actu->suivant->valeur == val)
+    {
+      element_t *val_del = el_actu->suivant;
+      el_actu->suivant = val_del->suivant;
+      free(val_del);
+      break;
+    }
+    el_actu = el_actu->suivant;
+  }
+  return element;
+}
+
+element_t *Supprime_element_ensemble(element_t *element, int val)
+{
+    if (element == NULL) {
+        return NULL;
+    }
+
+    element_t *is_there = Recherche_val(element, val);
+    if (is_there != NULL) {
+        if (is_there->frequence == 1) {
+            element = Supprime_total_element_ensemble(element, val);
+        } else {
+            is_there->frequence--;
+        }
+    }
+
+    return element;
+}
+
+element_t *Ajout_ensemble_trie(element_t *ensemble, int val, int freq)
+{
+  if (ensemble == NULL)
+  {
+    ensemble = malloc(sizeof(element_t));
+    ensemble->valeur = val;
+    ensemble->frequence = freq;
+    return ensemble;
+  }
+  element_t *is_there = Recherche_val(ensemble, val);
+  if (is_there == NULL)
+  {
+    element_t *nouvelle_valeur = malloc(sizeof(element_t));
+    if (val < ensemble->valeur)
+    {
+      nouvelle_valeur->valeur = val;
+      nouvelle_valeur->frequence = freq;
+      nouvelle_valeur->suivant = ensemble;
+      return nouvelle_valeur;
+    }
+    else
+    {
+      element_t *actuel = ensemble;
+      while (actuel->suivant != NULL && actuel->suivant->valeur < val) {
+        actuel = actuel->suivant;
+    }
+    
+      nouvelle_valeur->valeur = val;
+      nouvelle_valeur->frequence = freq;
+      nouvelle_valeur->suivant = actuel->suivant;
+      actuel->suivant = nouvelle_valeur;
+      return ensemble;
+    }
+  }
+  else {
+    is_there->frequence = is_there->frequence + freq;
+    return ensemble;
+  }
+}
+
+element_t *Supprime_element_ensemble_trie(element_t *liste, int val) {
+  if (liste == NULL) return NULL;
+
+  // Cas particulier : la valeur est en tête
+  if (liste->valeur == val) {
+      if (liste->frequence > 1) {
+          liste->frequence--;
+          return liste;
+      } else {
+          element_t *tmp = liste->suivant;
+          free(liste);
+          return tmp;
+      }
+  }
+
+  element_t *courant = liste;
+  while (courant->suivant != NULL && courant->suivant->valeur <= val) {
+      if (courant->suivant->valeur == val) {
+          if (courant->suivant->frequence > 1) {
+              courant->suivant->frequence--;
+          } else {
+              element_t *a_supprimer = courant->suivant;
+              courant->suivant = a_supprimer->suivant;
+              free(a_supprimer);
+          }
+          return liste;
+      }
+      courant = courant->suivant;
+  }
+
+  return liste; // valeur non trouvée
+}
+
+element_t *Supprime_total_element_ensemble_trie(element_t *liste, int val) {
+  if (liste == NULL) return NULL;
+
+  // Cas particulier : suppression en tête
+  if (liste->valeur == val) {
+      element_t *tmp = liste->suivant;
+      free(liste);
+      return tmp;
+  }
+
+  element_t *courant = liste;
+  while (courant->suivant != NULL && courant->suivant->valeur <= val) {
+      if (courant->suivant->valeur == val) {
+          element_t *a_supprimer = courant->suivant;
+          courant->suivant = a_supprimer->suivant;
+          free(a_supprimer);
+          return liste;
+      }
+      courant = courant->suivant;
+  }
+
+  return liste; // valeur non trouvée
 }
--- info 2/tme/semaine9/multi_ensembles.h
+++ info 2/tme/semaine9/multi_ensembles.h
@@ -8,6 +8,9 @@ struct _element_t{
 element_t *Recherche_val(element_t *ensemble, int val);
 element_t *Ajout_tete_ensemble(element_t *ensemble, int val, int freq);
 void Affiche_ensemble(element_t *ensemble);
-element_t *Creation_ensemble(int n);
-element_t *Supprime_total_element_ensemble(element_t *ensemble, int val);
-element_t *Supprime_element_ensemble(element_t *ensemble, int val);
+element_t * Creation_ensemble(int n);
+element_t *Supprime_total_element_ensemble(element_t *element, int val);
+element_t *Supprime_element_ensemble(element_t *element, int val);
+element_t *Ajout_ensemble_trie(element_t *ensemble, int val, int freq);
+element_t *Supprime_element_ensemble_trie(element_t *liste, int val);
+element_t *Supprime_total_element_ensemble_trie(element_t *liste, int val);

17c38875ef1b84cb34593978732a228a250f8dce
Paradise-Dami <87902304+Paradise-Dami@users.noreply.github.com>
2025-09-29 23:27:53 +0200

    Avancements sur TME 3

--- info 2/L2/TME-3-4-5/entreeSortieLC.c
+++ info 2/L2/TME-3-4-5/entreeSortieLC.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 
 Biblio *charger_n_entrees(char *nomfic, int n)
-{   
+{
     char *ligne[256];
     char *auteur[256];
     char *nom[256];
-    
+    int id;
+    Biblio *b = creer_biblio;
+
     FILE *fichier = fopen(nomfic, "r");
     if (fichier == NULL)
     {
@@ -16,11 +18,32 @@ Biblio *charger_n_entrees(char *nomfic, int n)
     }
 
     int i = 0;
-    
-    while ((fgets(ligne, 256, fichier) != NULL) && (i < n)){ // je pourrais utiliser les id dans les lignes...
-        i++;
 
+    while ((fgets(ligne, 256, fichier) != NULL) && (i < n))
+    { // je pourrais utiliser les id dans les lignes...
+        sscanf(ligne, "%d %s %s", id, nom, auteur);
+        i++;
+        inserer_en_tete(b, id, nom, auteur);
+        printf("%d %s %s", id, nom, auteur);
     }
+    fclose(fichier);
+    return b;
 }
 
-void enregistrer_biblio(Biblio *b, char *nomfic) {}
+void enregistrer_biblio(Biblio *b, char *nomfic)
+{
+    FILE *fichier = fopen(nomfic, "w");
+    if (fichier == NULL)
+    {
+        printf("erreur fichier introuvable");
+        return NULL;
+    }
+    Livre *actuel = b->L;
+    while (actuel != NULL)
+    {
+        fprintf(fichier, "%d %s %s", actuel->num, actuel->titre, actuel->auteur);
+        fputc("\n", fichier);
+        actuel = actuel->suiv;
+    }
+    fclose(fichier);
+}
